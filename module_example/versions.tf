d79bb254508a2b5911b842e08c26ef9950dd24ce
Laurent Brack <laurent.brack@gmail.com>
2021-03-06 10:48:31 -0800

    added terraform cloud backend

--- module_example/versions.tf
+++ module_example/versions.tf
@@ -5,6 +5,14 @@ terraform {
       version = "> 3.28.0"
     }
   }
-
   required_version = ">= 0.14.0"
+
+  backend "remote" {
+    hostname      = "app.terraform.io"
+    organization  = "periscopai"
+
+    workspaces {
+      name = "vivaterra-module-examples"
+    }
+  }
 }
